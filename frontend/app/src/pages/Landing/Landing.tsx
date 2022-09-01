c4d51440302d02256946f254710aa712dd73c6c5
Rosario Santa Marina <rosariosantamarina1@gmail.com>
2022-09-01 10:34:13 -0300

    3bp8hc0 Added ContactList component to ui and render it from app

--- frontend/app/src/pages/Landing/Landing.tsx
+++ frontend/app/src/pages/Landing/Landing.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
 import FullWidthLayout from 'layouts/FullWidth';
 import { useTranslation } from 'react-i18next';
+import { ContactList } from '@meethead/ui';
+
 
 const Landing: FC = () => {
   const { t } = useTranslation(['config', 'translations']);
@@ -8,6 +10,7 @@ const Landing: FC = () => {
   return (
     <FullWidthLayout>
       <h1>{t('app.title')}</h1>
+      <ContactList />
     </FullWidthLayout>
   );
 };
